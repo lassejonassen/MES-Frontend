39719797e148b691cf3b678ff85bcd9572d24afb
Lasse Jonassen <lasjn@arlafoods.com>
2025-07-28 00:31:57 +0200

    Added create and edit dialogs

---
+++ src/app/features/templates/components/template-details/create-template-property-dialog/create-template-property-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-template-property-dialog',
+  imports: [],
+  templateUrl: './create-template-property-dialog.component.html',
+  styleUrl: './create-template-property-dialog.component.scss'
+})
+export class CreateTemplatePropertyDialogComponent {
+
+}
---
+++ src/app/features/templates/components/template-details/edit-template-property-dialog/edit-template-property-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-template-property-dialog',
+  imports: [],
+  templateUrl: './edit-template-property-dialog.component.html',
+  styleUrl: './edit-template-property-dialog.component.scss'
+})
+export class EditTemplatePropertyDialogComponent {
+
+}
